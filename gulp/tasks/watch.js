f2e17567d03636c96d6243e5b7adb82133beb678
hilmerx <joel.hilme@gmail.com>
2017-08-23 22:39:58 +0200

    set up gulp enviroment

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,33 @@
+let gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create()
+
+
+gulp.task('browserSync', () => {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+    notify: false,
+    open: false
+  })
+})
+
+// watch function
+
+gulp.task('watch', ['browserSync'], () => {
+
+  watch('./app/css/**/*.css', () => { gulp.start('cssInject') })
+  watch('./app/js/**/*.js', () => {
+    console.log("hej")
+    browserSync.reload()
+  } )
+  watch('./app/index.html', browserSync.reload )
+
+})
+
+// cssInject function
+gulp.task('cssInject', ['styles'], () => {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream())
+})
